4ca7f64c906fbd6c85dd780060be6299c21372a6
fengshuo <fengshuo142003@gmail.com>
2015-06-27 15:21:16 +0800

    add vendors so that they can be required

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,7 @@ require('../style/style.less')
 var margins = require("./components/margin.js");
 
 margins("hooray")
+
+var d3 = require('d3');
+
+console.log(d3.select("body"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
-module.exports = {
+var bower_dir = __dirname + '/bower_components';
+
+var config = {
+	addVendor: function(name, path){
+		this.resolve.alias[name] = path;
+		this.module.noParse.push(path);
+	},
 	entry: "./app/js/main.js",
 	output: {
 		"path": "./build",
 		"filename": "bundle.js"
 	},
 	module: {
+		noParse: [],
 		loaders: [
 			{test: /\.less$/, loader: 'style!css!less'}
 		]
+	},
+	resolve: {
+		alias: {}
 	}
 };
+
+config.addVendor('jquery', bower_dir + '/jquery/dist/jquery.min.js');
+config.addVendor('d3', bower_dir + '/d3/d3.min.js');
+
+module.exports = config;
