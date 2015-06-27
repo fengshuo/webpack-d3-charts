f8405b9d3a36aaee923d95ecd2db6073c088548f
fengshuo <fengshuo142003@gmail.com>
2015-06-27 16:24:03 +0800

    add margins convention module

--- app/js/components/margin.js
+++ app/js/components/margin.js
@@ -1,5 +1,66 @@
-var marginConvention = function(x){
-	console.log("margins conventions " + x)
+/*
+ * Implement margin conventions
+ * http://bl.ocks.org/mbostock/3019563
+ */
+
+
+function marginConvention(){
+	var svg;
+	var defaults = {
+		margin:{
+			top: 20,
+			right: 20,
+			bottom: 30,
+			left: 30
+		},
+		width: 960,
+		height: 600
+	};
+
+	function chart(selection){
+		var _this = this;
+		selection.each(function(d,i){
+			svg = d3.select(this)
+				.attr({
+					"width": defaults.width,
+					"height": defaults.height
+				})
+				.append("g")
+				.attr("transform","translate(" + defaults.margin.left + "," + defaults.margin.top + ")");
+
+			_this.innerChart = svg;
+		})
+
+	}
+
+	chart.width = function(value){
+		if(!arguments.length) return defaults.width;
+
+		defaults.width = value;
+		return chart;
+	};
+
+	chart.height = function(value){
+		if(!arguments.length) return defaults.height;
+
+		defaults.height = value;
+		return chart;
+	};
+
+	chart.margin = function(value){
+		if(!arguments.length) return defaults.height;
+
+		defaults.margin = value;
+		return chart;
+	};
+
+	chart.innerWidth = defaults.width - defaults.margin.left - defaults.margin.right;
+	chart.innerHeight = defaults.height - defaults.margin.top - defaults.margin.bottom;
+
+
+	return chart;
+
 };
+
 
 module.exports = marginConvention;
--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,30 @@
-// styles
+/* load styles */
 require('../style/style.less')
 
-// js
-var margins = require("./components/margin.js");
+/* javascripts */
+var data = [ { label: "Data Set 1",
+	x: [0, 1, 2, 3, 4],
+	y: [0, 1, 2, 3, 4] },
+	{ label: "Data Set 2",
+		x: [0, 1, 2, 3, 4],
+		y: [0, 1, 4, 9, 16] } ] ;
 
-margins("hooray")
+var d3Chart = require('./components/margin.js');
 
 
 
-console.log(d3.select("body"))
+var chart = d3Chart().width(800).height(500)
+	.margin({
+		top:10,
+		right:20,
+		bottom:30,
+		left:30
+	});
+
+var svg = d3.select("body").append("svg")
+	.datum(data)
+	.call(chart);
+
+svg.innerChart.append("g")
+	.attr("transform","translate(0,"+chart.innerHeight+")")
+	.attr("class","axis x")
